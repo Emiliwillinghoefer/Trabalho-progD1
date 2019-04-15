ec891d771831ed06746e20468c710afc64e351eb
emiliwillinghoefer <emili_willi@hotmail.com>
2019-04-15 14:34:24 -0300

    Parte 1

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main{
+	public static void main(String[] args){
+		Numeros a=new Numeros();
+		Scanner s = new Scanner(System.in);
+		System.out.print("Dia:");
+		a.dia = s.nextInt();
+		System.out.print("Mês:");
+		a.ano = s.nextInt();
+		System.out.print("Horas:");
+		a.horas = s.nextInt();
+		System.out.print("Minutos:");
+		a.minutos = s.nextInt();
+
+		System.out.println("\nDia ="+a.dia+"\nAno ="+a.ano+"\nHoras ="+a.horas+":"+a.minutos);
+	}
+	
+}
---
+++ Numeros.java
@@ -0,0 +1,11 @@
+class Numeros{
+	public int dia;
+	public int ano;
+	public int horas;
+	public int minutos;
+
+	public Numeros(){
+
+	}
+		
+}
